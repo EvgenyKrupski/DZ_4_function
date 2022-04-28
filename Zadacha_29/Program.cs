30542bee391e87f1dde6605b69f4694a62dd7013
EvgenyKrupski <Krupev87@yandex.ru>
2022-04-28 09:53:56 +0300

    Решение задачи 29 семинара 4

--- Zadacha_29/Program.cs
+++ Zadacha_29/Program.cs
@@ -3,12 +3,37 @@
 //6, 1, 33 -> [6, 1, 33]
 //Поправка: на семинаре дал добро на заволнение массива рандомными числами. В оригинале нужно пользователю ввести строку - ряд чисел через запятую, а программа превращает эту строку в полноценный массив и выводит. Рекомендую вторым вариантом)
 
-Console.Write("Введите числа массива через запятую: ");
+Console.Write("Введите числа через запятую: ");
 string numbers = Console.ReadLine();
-numbers = numbers.Replace(" ", "");
-string[] arrayNumbers = numbers.Split(new char[] {','});
 
-for(int i = 0; i < arrayNumbers.Length; i++)
-{
-    Console.Write($"{arrayNumbers[i]} ");
+int countNumbers = 1;
+
+for(int i = 0; i < numbers.Length; i++){
+    if(numbers[i] == ','){
+        countNumbers++;
+    }
+}
+
+int[] numersArr = new int[countNumbers];
+int index = 0;
+
+for(int i = 0; i < numbers.Length; i++){
+    string tempString = "";
+    
+    while(numbers[i] != ','){
+        if(i != numbers.Length - 1){
+            tempString += numbers[i].ToString();
+            i++;
+        }
+        else{
+            tempString += numbers[i].ToString();
+            break;
+        }
+    }
+    numersArr[index] = Convert.ToInt32(tempString);
+    index++;
+}
+
+for(int i = 0; i < numersArr.Length; i++){
+    Console.Write(numersArr[i] + " ");
 }
